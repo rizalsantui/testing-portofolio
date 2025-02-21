fb9a67521bc648a550ea103aeedaf87c58c7efc4
rizalsantui <rizalsantui10@gmail.com>
2025-02-21 22:31:51 +0700

    Update script.js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Contoh JavaScript sederhana untuk form kontak
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Pesan Anda telah terkirim!');
+    // Di sini kamu bisa menambahkan logika untuk mengirim data form ke server
+});
